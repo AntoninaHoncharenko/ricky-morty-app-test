e93d5223f5bf9afbc39ab35950a562321f87d9ca
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2023-03-17 19:31:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { AuthContextProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ricky-morty-app-test">
+    <BrowserRouter>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
